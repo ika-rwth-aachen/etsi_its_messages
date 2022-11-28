7e7c5a5f03f2b9a7ec38bc3e6bb68668d6c377ee
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2022-11-28 23:34:54 +0100

    add first version of script to reduce asn1 to dependencies

---
+++ etsi_its_msgs/scripts/reduceAsn1ToTypeDependencies.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+from typing import Dict, List
+
+import asn1tools
+
+
+def parseCli():
+
+    parser = argparse.ArgumentParser(
+        description="Reduces given ASN1 files to the type dependencies of a given type.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("files", type=str, nargs="+", help="ASN1 files")
+    parser.add_argument("-t", "--type", type=str, required=True, help="ASN1 type")
+    parser.add_argument("-o", "--output-dir", type=str, required=True, help="output directory")
+
+    args = parser.parse_args()
+
+    return args
+
+
+def parseAsn1Files(files: List[str]) -> Dict:
+
+    return asn1tools.parse_files(files)
+
+
+def findTypeDependencies(docs: Dict, type: str) -> Dict[str, Dict]:
+
+    relevant_type_infos = {}
+
+    # find given type
+    type_info = None
+    for doc, info in docs.items():
+        if type in info["types"].keys():
+            type_info = info["types"][type]
+            break
+    if type_info is None:
+        return {}
+    relevant_type_infos[type] = type_info
+
+    # get dependency types
+    member_types = []
+    if "members" in type_info:
+        member_types = [m["type"] for m in type_info["members"] if m is not None]
+
+    # recursively find dependencies of dependencies
+    for member_type in member_types:
+        rti = findTypeDependencies(docs, member_type)
+        relevant_type_infos.update(rti)
+    
+    return relevant_type_infos
+
+
+def reduceAsn1Files(files: List[str], types: List[str], output_dir: str):
+
+    for file in files:
+
+        # read file and reduce
+        with open(file, "r") as f:
+            lines = f.readlines()
+        reduced_lines = reduceAsn1File(lines, types)
+
+        # write reduced file
+        new_file = os.path.join(output_dir, os.path.basename(file))
+        os.makedirs(output_dir, exist_ok=True)
+        with open(new_file, "w") as f:
+            for line in reduced_lines:
+                f.write(line)
+
+
+def reduceAsn1File(lines: List[str], types: List[str]) -> List[str]:
+
+    reduced_lines = []
+    copying = True # start copying header
+    copying_type = False
+    brace_level = 0
+
+    # loop over all lines to check which ones to keep
+    for line in lines:
+
+        # always copy empty lines and keyword lines
+        if line.isspace() or line.strip() in ("BEGIN", "END"):
+            reduced_lines.append(line)
+            continue
+
+        # detect type to keep, entering copy mode
+        if "::=" in line:
+            if line.split("::=")[0].strip() in types:
+                copying_type = True
+                copying = False
+
+        # copy line
+        if copying_type or copying:
+            reduced_lines.append(line)
+
+        # leave copy mode when curly braces are closed
+        brace_level += line.count("{")
+        brace_level -= line.count("}")
+        if brace_level == 0:
+            copying_type = False
+
+    return reduced_lines
+
+
+def main():
+
+    args = parseCli()
+
+    docs = parseAsn1Files(args.files)
+
+    relevant_type_infos = findTypeDependencies(docs, args.type)
+    for type in sorted(relevant_type_infos):
+        print(type)
+
+    reduceAsn1Files(args.files, list(relevant_type_infos.keys()), args.output_dir)
+
+
+if __name__ == "__main__":
+    
+    main()
