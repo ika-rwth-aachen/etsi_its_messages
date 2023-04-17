716446e3f01b270f6f9621bfdf6f087b3755b3f9
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-04-17 22:28:53 +0200

    add first jinja templating demo script

---
+++ utils/scripts/jinja/jinja.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from jinja2 import Environment, FileSystemLoader
+
+max_score = 100
+test_name = "Python Challenge"
+students = [
+    {"name": "Sandrine",  "score": 100},
+    {"name": "Gergeley", "score": 87},
+    {"name": "Frieda", "score": 92},
+]
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template("message.txt")
+
+for student in students:
+    filename = f"output/message_{student['name'].lower()}.txt"
+    content = template.render(
+        student,
+        max_score=max_score,
+        test_name=test_name
+    )
+    with open(filename, mode="w", encoding="utf-8") as message:
+        message.write(content)
+        print(f"... wrote {filename}")
