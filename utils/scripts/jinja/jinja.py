e2bfbe36a6fcc7f48229441d56cdc510aec94e85
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-04-17 23:00:08 +0200

    implement demo jinja script for msg files

--- utils/scripts/jinja/jinja.py
+++ utils/scripts/jinja/jinja.py
@@ -2,24 +2,45 @@
 
 from jinja2 import Environment, FileSystemLoader
 
-max_score = 100
-test_name = "Python Challenge"
-students = [
-    {"name": "Sandrine",  "score": 100},
-    {"name": "Gergeley", "score": 87},
-    {"name": "Frieda", "score": 92},
+
+asn1_definition = "multi\nline\ntest"
+
+members = [
+    {
+        "type": "uint8",
+        "name": "choice",
+    }, {
+        "type": "BasicVehicleContainerHighFrequency",
+        "name": "basicVehicleContainerHighFrequency",
+        "constants": [
+            {
+                "type": "uint8",
+                "name": "CHOICE_BASIC_VEHICLE_CONTAINER_HIGH_FREQUENCY",
+                "value": "0",
+            }
+        ]
+    }, {
+        "type": "RSUContainerHighFrequency",
+        "name": "rsuContainerHighFrequency",
+        "constants": [
+            {
+                "type": "uint8",
+                "name": "CHOICE_RSU_CONTAINER_HIGH_FREQUENCY",
+                "value": "1",
+            }
+        ]
+    },
 ]
 
-environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("message.txt")
 
-for student in students:
-    filename = f"output/message_{student['name'].lower()}.txt"
-    content = template.render(
-        student,
-        max_score=max_score,
-        test_name=test_name
-    )
-    with open(filename, mode="w", encoding="utf-8") as message:
-        message.write(content)
-        print(f"... wrote {filename}")
+environment = Environment(loader=FileSystemLoader("templates/"), trim_blocks=False)
+template = environment.get_template("RosMessageType.msg")
+context = {
+    "asn1_definition": asn1_definition,
+    "members": members,
+}
+
+content = template.render(context)
+
+with open("output/HighFrequencyContainer.msg", "w") as f:
+    f.write(content)
