8a87eb204f662607106a947b1105b2b81416969e
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-07-04 23:16:36 +0200

    add logging to converter

--- etsi_its_conversion/etsi_its_conversion/src/Converter.cpp
+++ etsi_its_conversion/etsi_its_conversion/src/Converter.cpp
@@ -24,10 +24,14 @@ void Converter::onInit() {
   publishers_asn1_["cam"] = private_node_handle_.advertise<bitstring_msgs::UInt8ArrayStamped>(kOutputTopicAsn1Cam, 1);
   subscribers_["cam"] = private_node_handle_.subscribe(kInputTopicCam, 1, &Converter::rosCallbackCam, this);
   subscribers_asn1_["cam"] = private_node_handle_.subscribe(kInputTopicAsn1Cam, 1, &Converter::asn1CallbackCam, this);
+  NODELET_INFO("Converting CAM bitstrings on '%s' to CAM messages on '%s'", subscribers_asn1_["cam"].getTopic().c_str(), publishers_["cam"].getTopic().c_str());
+  NODELET_INFO("Converting CAM messages on '%s' to CAM bitstrings on '%s'", subscribers_["cam"].getTopic().c_str(), publishers_asn1_["cam"].getTopic().c_str());
 }
 
 
 void Converter::asn1CallbackCam(const bitstring_msgs::UInt8ArrayStamped::ConstPtr bitstring_msg) {
+
+  NODELET_DEBUG("Received CAM bitstring");
 
   // decode ASN1 bitstring to struct
   CAM_t* asn1_struct;
@@ -36,20 +40,25 @@ void Converter::asn1CallbackCam(const bitstring_msgs::UInt8ArrayStamped::ConstPt
     NODELET_ERROR("Failed to decode message");
     return;
   }
+  // asn_fprint(stdout, &asn_DEF_CAM, asn1_struct);
 
   // convert struct to ROS msg
   etsi_its_cam_msgs::CAM msg;
   etsi_its_cam_conversion::toRos_CAM(*asn1_struct, msg);
 
   publishers_["cam"].publish(msg);
+  NODELET_DEBUG("Published CAM");
 }
 
 
 void Converter::rosCallbackCam(const etsi_its_cam_msgs::CAM::ConstPtr msg) {
 
+  NODELET_DEBUG("Received CAM");
+
   // convert ROS msg to struct
   CAM_t asn1_struct;
   etsi_its_cam_conversion::toStruct_CAM(*msg, asn1_struct);
+  // asn_fprint(stdout, &asn_DEF_CAM, &asn1_struct);
 
   // encode struct to ASN1 bitstring
   char error_buffer[1024];
@@ -64,14 +73,13 @@ void Converter::rosCallbackCam(const etsi_its_cam_msgs::CAM::ConstPtr msg) {
     NODELET_ERROR("Failed to encode message: %s", ret.result.failed_type->xml_tag);
     return;
   }
-  
-  // TODO: debug statements
 
   bitstring_msgs::UInt8ArrayStamped bitstring_msg;
   bitstring_msg.header.stamp = ros::Time::now(); // TODO: use msg time?
   bitstring_msg.header.frame_id = ""; // TODO
   bitstring_msg.data = std::vector<uint8_t>((uint8_t*)ret.buffer, (uint8_t*)ret.buffer + (int)ret.result.encoded);
   publishers_asn1_["cam"].publish(bitstring_msg);
+  NODELET_DEBUG("Published CAM bitstring");
 }
 
 
