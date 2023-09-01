85d729eee12c7406c79f24c57481c1c63855a3bb
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-09-01 12:41:26 +0200

    swap ifdef statements in etsi_its_conversion

--- etsi_its_conversion/etsi_its_conversion/include/etsi_its_conversion/Converter.hpp
+++ etsi_its_conversion/etsi_its_conversion/include/etsi_its_conversion/Converter.hpp
@@ -5,33 +5,33 @@
 #include <unordered_map>
 
 #include <etsi_its_cam_conversion/convertCAM.h>
-#ifndef ROS1
-#include <etsi_its_cam_msgs/msg/cam.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <udp_msgs/msg/udp_packet.hpp>
-#else
+#ifdef ROS1
 #include <nodelet/nodelet.h>
 #include <ros/ros.h>
 #include <udp_msgs/UdpPacket.h>
 #include <etsi_its_cam_msgs/CAM.h>
+#else
+#include <etsi_its_cam_msgs/msg/cam.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <udp_msgs/msg/udp_packet.hpp>
 #endif
 
 
 namespace etsi_its_conversion {
 
 
-#ifndef ROS1
-class Converter : public rclcpp::Node {
-#else
+#ifdef ROS1
 class Converter : public nodelet::Nodelet {
+#else
+class Converter : public rclcpp::Node {
 #endif
 
   public:
 
-#ifndef ROS1
-    explicit Converter(const rclcpp::NodeOptions& options);
-#else
+#ifdef ROS1
     virtual void onInit();
+#else
+    explicit Converter(const rclcpp::NodeOptions& options);
 #endif
 
   protected:
@@ -40,16 +40,16 @@ class Converter : public nodelet::Nodelet {
 
     void setup();
 
-#ifndef ROS1
-    void udpCallback(const udp_msgs::msg::UdpPacket::UniquePtr udp_msg);
-#else
+#ifdef ROS1
     void udpCallback(const udp_msgs::UdpPacket::ConstPtr udp_msg);
+#else
+    void udpCallback(const udp_msgs::msg::UdpPacket::UniquePtr udp_msg);
 #endif
 
-#ifndef ROS1
-    void rosCallbackCam(const etsi_its_cam_msgs::msg::CAM::UniquePtr msg);
-#else
+#ifdef ROS1
     void rosCallbackCam(const etsi_its_cam_msgs::CAM::ConstPtr msg);
+#else
+    void rosCallbackCam(const etsi_its_cam_msgs::msg::CAM::UniquePtr msg);
 #endif
 
   protected:
@@ -64,17 +64,17 @@ class Converter : public nodelet::Nodelet {
 
     std::string etsi_type_;
 
-#ifndef ROS1
-    rclcpp::Subscription<udp_msgs::msg::UdpPacket>::SharedPtr subscriber_udp_;
-    std::unordered_map<std::string, rclcpp::Publisher<etsi_its_cam_msgs::msg::CAM>::SharedPtr> publishers_;
-    std::unordered_map<std::string, rclcpp::Subscription<etsi_its_cam_msgs::msg::CAM>::SharedPtr> subscribers_;
-    rclcpp::Publisher<udp_msgs::msg::UdpPacket>::SharedPtr publisher_udp_;
-#else
+#ifdef ROS1
     ros::NodeHandle private_node_handle_;
     ros::Subscriber subscriber_udp_;
     std::unordered_map<std::string, ros::Publisher> publishers_;
     std::unordered_map<std::string, ros::Subscriber> subscribers_;
     ros::Publisher publisher_udp_;
+#else
+    rclcpp::Subscription<udp_msgs::msg::UdpPacket>::SharedPtr subscriber_udp_;
+    std::unordered_map<std::string, rclcpp::Publisher<etsi_its_cam_msgs::msg::CAM>::SharedPtr> publishers_;
+    std::unordered_map<std::string, rclcpp::Subscription<etsi_its_cam_msgs::msg::CAM>::SharedPtr> subscribers_;
+    rclcpp::Publisher<udp_msgs::msg::UdpPacket>::SharedPtr publisher_udp_;
 #endif
 
 };
