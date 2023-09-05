cfb103208ff3e0427df2fcd46d87fb881b140230
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2023-09-05 21:01:45 +0200

    convert `OCTET_STRING` according to `BIT_STRING`

--- etsi_its_conversion/etsi_its_cam_conversion/include/etsi_its_cam_conversion/primitives/convertOCTET_STRING.h
+++ etsi_its_conversion/etsi_its_cam_conversion/include/etsi_its_cam_conversion/primitives/convertOCTET_STRING.h
@@ -1,13 +1,21 @@
 #pragma once
 
 #include <etsi_its_cam_coding/OCTET_STRING.h>
-#include <string>
+#include <cstring>
 
 namespace etsi_its_cam_conversion {
 
-  void toRos_OCTET_STRING(const OCTET_STRING_t& _OCTET_STRING_in, std::string& OCTET_STRING_out) {
-    for (int i = 0; i < _OCTET_STRING_in.size; i++)
-      OCTET_STRING_out.push_back(_OCTET_STRING_in.buf[i]);
+
+  void toRos_OCTET_STRING(const OCTET_STRING_t& _OCTET_STRING_in, std::vector<uint8_t>& OCTET_STRING_out) {
+    OCTET_STRING_out.resize(_OCTET_STRING_in.size);
+    std::memcpy(OCTET_STRING_out.data(), _OCTET_STRING_in.buf, _OCTET_STRING_in.size);
+  }
+
+  void toStruct_OCTET_STRING(const std::vector<uint8_t>& _OCTET_STRING_in, OCTET_STRING_t& OCTET_STRING_out) {
+
+    OCTET_STRING_out.size = _OCTET_STRING_in.size();
+    OCTET_STRING_out.buf = new uint8_t[OCTET_STRING_out.size];
+    std::memcpy(OCTET_STRING_out.buf, _OCTET_STRING_in.data(), OCTET_STRING_out.size);
   }
 
   void toStruct_OCTET_STRING(const std::string& _OCTET_STRING_in, OCTET_STRING_t& OCTET_STRING_out) {
