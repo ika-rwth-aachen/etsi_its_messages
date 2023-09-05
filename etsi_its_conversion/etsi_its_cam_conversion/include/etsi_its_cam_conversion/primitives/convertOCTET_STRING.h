8e75ab31849893d51a0cfcdcba27c6c3ad8a0851
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-09-05 22:32:49 +0200

    add exception to toStruct_OCTET_STRING

--- etsi_its_conversion/etsi_its_cam_conversion/include/etsi_its_cam_conversion/primitives/convertOCTET_STRING.h
+++ etsi_its_conversion/etsi_its_cam_conversion/include/etsi_its_cam_conversion/primitives/convertOCTET_STRING.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include <etsi_its_cam_coding/OCTET_STRING.h>
 #include <cstring>
+
+#include <etsi_its_cam_coding/OCTET_STRING.h>
+
 
 namespace etsi_its_cam_conversion {
 
-
   void toRos_OCTET_STRING(const OCTET_STRING_t& _OCTET_STRING_in, std::vector<uint8_t>& OCTET_STRING_out) {
+
     OCTET_STRING_out.resize(_OCTET_STRING_in.size);
     std::memcpy(OCTET_STRING_out.data(), _OCTET_STRING_in.buf, _OCTET_STRING_in.size);
   }
@@ -19,8 +21,10 @@ namespace etsi_its_cam_conversion {
   }
 
   void toStruct_OCTET_STRING(const std::string& _OCTET_STRING_in, OCTET_STRING_t& OCTET_STRING_out) {
+
     const char *cstr = _OCTET_STRING_in.c_str();
-    int ret_v = OCTET_STRING_fromString(&OCTET_STRING_out, cstr);
+    int status = OCTET_STRING_fromString(&OCTET_STRING_out, cstr);
+    if (status != 0) throw std::invalid_argument("Failed to convert string to OCTET STRING");
   }
 
 }
