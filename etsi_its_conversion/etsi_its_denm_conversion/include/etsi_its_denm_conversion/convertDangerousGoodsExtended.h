77fdc70b9b931f469ffb89828bb04d1a30b5e7f1
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2023-10-10 00:00:10 +0200

    fix primitives handling in SEQUENCE

--- etsi_its_conversion/etsi_its_denm_conversion/include/etsi_its_denm_conversion/convertDangerousGoodsExtended.h
+++ etsi_its_conversion/etsi_its_denm_conversion/include/etsi_its_denm_conversion/convertDangerousGoodsExtended.h
@@ -2,13 +2,13 @@
 
 #include <etsi_its_denm_coding/DangerousGoodsExtended.h>
 #include <etsi_its_denm_conversion/convertDangerousGoodsBasic.h>
-#include <etsi_its_denm_conversion/primitives/convertINTEGER.h>
-#include <etsi_its_denm_conversion/primitives/convertBOOLEAN.h>
-#include <etsi_its_denm_conversion/primitives/convertBOOLEAN.h>
-#include <etsi_its_denm_conversion/primitives/convertBOOLEAN.h>
-#include <etsi_its_denm_conversion/primitives/convertIA5String.h>
+#include <etsi_its_primitives_conversion/convertINTEGER.h>
+#include <etsi_its_primitives_conversion/convertBOOLEAN.h>
+#include <etsi_its_primitives_conversion/convertBOOLEAN.h>
+#include <etsi_its_primitives_conversion/convertBOOLEAN.h>
+#include <etsi_its_primitives_conversion/convertIA5String.h>
 #include <etsi_its_denm_conversion/convertPhoneNumber.h>
-#include <etsi_its_denm_conversion/primitives/convertUTF8String.h>
+#include <etsi_its_primitives_conversion/convertUTF8String.h>
 #ifdef ROS1
 #include <etsi_its_denm_msgs/DangerousGoodsExtended.h>
 namespace denm_msgs = etsi_its_denm_msgs;
@@ -23,12 +23,12 @@ namespace etsi_its_denm_conversion {
 void toRos_DangerousGoodsExtended(const DangerousGoodsExtended_t& in, denm_msgs::DangerousGoodsExtended& out) {
 
   toRos_DangerousGoodsBasic(in.dangerousGoodsType, out.dangerous_goods_type);
-  toRos_INTEGER(in.unNumber, out.un_number);
-  toRos_BOOLEAN(in.elevatedTemperature, out.elevated_temperature);
-  toRos_BOOLEAN(in.tunnelsRestricted, out.tunnels_restricted);
-  toRos_BOOLEAN(in.limitedQuantity, out.limited_quantity);
+  etsi_its_primitives_conversion::toRos_INTEGER(in.unNumber, out.un_number);
+  etsi_its_primitives_conversion::toRos_BOOLEAN(in.elevatedTemperature, out.elevated_temperature);
+  etsi_its_primitives_conversion::toRos_BOOLEAN(in.tunnelsRestricted, out.tunnels_restricted);
+  etsi_its_primitives_conversion::toRos_BOOLEAN(in.limitedQuantity, out.limited_quantity);
   if (in.emergencyActionCode) {
-    toRos_IA5String(*in.emergencyActionCode, out.emergency_action_code);
+    etsi_its_primitives_conversion::toRos_IA5String(*in.emergencyActionCode, out.emergency_action_code);
     out.emergency_action_code_is_present = true;
   }
 
@@ -38,7 +38,7 @@ void toRos_DangerousGoodsExtended(const DangerousGoodsExtended_t& in, denm_msgs:
   }
 
   if (in.companyName) {
-    toRos_UTF8String(*in.companyName, out.company_name);
+    etsi_its_primitives_conversion::toRos_UTF8String(*in.companyName, out.company_name);
     out.company_name_is_present = true;
   }
 
@@ -49,13 +49,13 @@ void toStruct_DangerousGoodsExtended(const denm_msgs::DangerousGoodsExtended& in
   memset(&out, 0, sizeof(DangerousGoodsExtended_t));
 
   toStruct_DangerousGoodsBasic(in.dangerous_goods_type, out.dangerousGoodsType);
-  toStruct_INTEGER(in.un_number, out.unNumber);
-  toStruct_BOOLEAN(in.elevated_temperature, out.elevatedTemperature);
-  toStruct_BOOLEAN(in.tunnels_restricted, out.tunnelsRestricted);
-  toStruct_BOOLEAN(in.limited_quantity, out.limitedQuantity);
+  etsi_its_primitives_conversion::toStruct_INTEGER(in.un_number, out.unNumber);
+  etsi_its_primitives_conversion::toStruct_BOOLEAN(in.elevated_temperature, out.elevatedTemperature);
+  etsi_its_primitives_conversion::toStruct_BOOLEAN(in.tunnels_restricted, out.tunnelsRestricted);
+  etsi_its_primitives_conversion::toStruct_BOOLEAN(in.limited_quantity, out.limitedQuantity);
   if (in.emergency_action_code_is_present) {
     IA5String_t emergency_action_code;
-    toStruct_IA5String(in.emergency_action_code, emergency_action_code);
+    etsi_its_primitives_conversion::toStruct_IA5String(in.emergency_action_code, emergency_action_code);
     out.emergencyActionCode = new IA5String_t(emergency_action_code);
   }
 
@@ -67,7 +67,7 @@ void toStruct_DangerousGoodsExtended(const denm_msgs::DangerousGoodsExtended& in
 
   if (in.company_name_is_present) {
     UTF8String_t company_name;
-    toStruct_UTF8String(in.company_name, company_name);
+    etsi_its_primitives_conversion::toStruct_UTF8String(in.company_name, company_name);
     out.companyName = new UTF8String_t(company_name);
   }
 
