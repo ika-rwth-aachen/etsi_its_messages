ac5c6ef80a4de68a926c1be426a6735f309133f9
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-10-21 00:32:37 +0200

    adjust denm test after renaming of some message fields

--- tmp-dev-utils/publish_denm.py
+++ tmp-dev-utils/publish_denm.py
@@ -15,12 +15,12 @@ def publish():
     msg = DENM()
 
     msg.header.protocol_version = 2
-    msg.header.message_id = msg.header.MESSAGE_I_D_DENM
- 
+    msg.header.message_id = msg.header.MESSAGE_ID_DENM
+
     msg.denm.alacarte_is_present = True
     msg.denm.alacarte.stationary_vehicle_is_present = True
     msg.denm.alacarte.stationary_vehicle.carrying_dangerous_goods_is_present = True
-    
+
     dangerous_goods_extended = DangerousGoodsExtended()
     dangerous_goods_extended.emergency_action_code_is_present = True
     dangerous_goods_extended.emergency_action_code = "22"  # IA5String
