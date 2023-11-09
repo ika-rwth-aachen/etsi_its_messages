29bbb66cb6fc4f63caec798a2e9ec5107dc8311e
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2023-11-09 07:11:40 +0100

    update comments in header files

--- etsi_its_msgs/etsi_its_msgs/include/etsi_its_msgs/cam_access.hpp
+++ etsi_its_msgs/etsi_its_msgs/include/etsi_its_msgs/cam_access.hpp
@@ -1,4 +1,5 @@
-/** ============================================================================
+/*
+=============================================================================
 MIT License
 
 Copyright (c) 2023 Institute for Automotive Engineering (ika), RWTH Aachen University
@@ -20,7 +21,13 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-============================================================================= */
+=============================================================================
+*/
+
+/**
+ * @file cam_access.hpp
+ * @brief Main object access header to include in ROS 2 projects
+ */
 
 #pragma once
 
--- etsi_its_msgs/etsi_its_msgs/include/etsi_its_msgs/impl/cdd/cdd_access.h
+++ etsi_its_msgs/etsi_its_msgs/include/etsi_its_msgs/impl/cdd/cdd_access.h
@@ -1,4 +1,5 @@
-/** ============================================================================
+/*
+=============================================================================
 MIT License
 
 Copyright (c) 2023 Institute for Automotive Engineering (ika), RWTH Aachen University
@@ -20,11 +21,12 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-============================================================================= */
+=============================================================================
+*/
 
 /**
- * @file
- * @brief Main CDD access header to include
+ * @file impl/cdd/cdd_access.h
+ * @brief Main CDD access header implementation
  */
 
 #pragma once
