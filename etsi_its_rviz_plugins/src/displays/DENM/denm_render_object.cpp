4e8c6256fe848b9ad4da46a7b47fe874dbd641d6
Cilli Danker <cilli.danker@ika.rwth-aachen.de>
2023-11-23 13:15:49 +0100

    get utm position from denm messages

--- etsi_its_rviz_plugins/src/displays/DENM/denm_render_object.cpp
+++ etsi_its_rviz_plugins/src/displays/DENM/denm_render_object.cpp
@@ -6,19 +6,31 @@ namespace displays
 {
 
   DENMRenderObject::DENMRenderObject(etsi_its_denm_msgs::msg::DENM denm, rclcpp::Time receive_time, uint16_t n_leap_seconds) {
-
+    //getUTMPosition()
     int zone;
     bool northp;
-    geometry_msgs::msg::PointStamped p = etsi_its_cam_msgs::access::getUTMPosition(cam, zone, northp);
+    geometry_msgs::msg::PointStamped p;
+    double latitude = (denm.denm.management.event_position.latitude.value)*1e-7;
+    double longitude = (denm.denm.management.event_position.longitude.value)*1e-7;
+    p.point.z = (denm.denm.management.event_position.altitude.altitude_value.value)*1e-2;
+    try {
+      GeographicLib::UTMUPS::Forward(latitude, longitude, zone, northp, p.point.x, p.point.y);
+      std::string hemisphere;
+      if(northp) hemisphere="N";
+      else hemisphere="S";
+      p.header.frame_id="utm_"+std::to_string(zone)+hemisphere;
+    } catch (GeographicLib::GeographicErr& e) {
+      throw std::invalid_argument(e.what());
+    }
     header.frame_id = p.header.frame_id;
     
     uint64_t nanosecs = etsi_its_denm_msgs::access::getUnixNanosecondsFromGenerationDeltaTime(etsi_its_denm_msgs::access::getGenerationDeltaTime(denm), receive_time.nanoseconds(), n_leap_seconds);
-    header.stamp = rclcpp::Time(nanosecs);
+        header.stamp = rclcpp::Time(nanosecs);
 
     station_id = etsi_its_denm_msgs::access::getStationID(denm);
     station_type = etsi_its_denm_msgs::access::getStationType(denm);
     
-    double heading = (90-etsi_its_denm_msgs::access::getHeading(denm))*M_PI/180.0; // 0.0° equals WGS84 North, 90.0° equals WGS84 East, 180.0° equals WGS84 South and 270.0° equals WGS84 West
+        double heading = (90-etsi_its_denm_msgs::access::getHeading(denm))*M_PI/180.0; // 0.0° equals WGS84 North, 90.0° equals WGS84 East, 180.0° equals WGS84 South and 270.0° equals WGS84 West
     while(heading<0) heading+=2*M_PI;
     pose.position = p.point;
     tf2::Quaternion orientation;
@@ -27,7 +39,7 @@ namespace displays
     
     dimensions.x = etsi_its_denm_msgs::access::getVehicleLength(denm);
     dimensions.y = etsi_its_denm_msgs::access::getVehicleWidth(denm);
-    dimensions.z = 1.6;
+        dimensions.z = 1.6;
 
     speed = etsi_its_denm_msgs::access::getSpeed(denm);
   }
@@ -51,7 +63,7 @@ namespace displays
   int DENMRenderObject::getStationID() {
     return station_id;
   }
-
+  
   int DENMRenderObject::getStationType() {
     return station_type;
   }
@@ -59,14 +71,14 @@ namespace displays
   geometry_msgs::msg::Pose DENMRenderObject::getPose() {
     return pose;
   }
-
+  
   geometry_msgs::msg::Vector3 DENMRenderObject::getDimensions() {
 
     return dimensions;
   }
 
   double DENMRenderObject::getSpeed() {
-    return speed;
+  return speed;
   }
 
 }  // namespace displays
