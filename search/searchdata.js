4906d2f1ea82a9972398bd3af28f49885dfcd720
lreiher <lreiher@users.noreply.github.com>
2023-12-05 16:11:27 +0000

    deploy: 1f4ee4ce6dc76c8e258b27277576ffa048ce69ea

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,9 +1,10 @@
 var indexSectionsWithContent =
 {
-  0: "cegs",
+  0: "cegls",
   1: "c",
   2: "gs",
-  3: "e"
+  3: "l",
+  4: "e"
 };
 
 var indexSectionNames =
@@ -11,7 +12,8 @@ var indexSectionNames =
   0: "all",
   1: "files",
   2: "functions",
-  3: "pages"
+  3: "variables",
+  4: "pages"
 };
 
 var indexSectionLabels =
@@ -19,6 +21,7 @@ var indexSectionLabels =
   0: "All",
   1: "Files",
   2: "Functions",
-  3: "Pages"
+  3: "Variables",
+  4: "Pages"
 };
 
