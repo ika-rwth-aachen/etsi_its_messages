1616c5ee1da728eadabde294d08c3d92f9d94c38
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2023-12-21 17:25:23 +0100

    improved tests

---
+++ etsi_its_msgs_utils/test/impl/test_j2735_access.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cmath>
+#include <limits>
+#include <chrono>
+#include <ctime>
+
+#include <gtest/gtest.h>
+
+#include "test_helpers.h"
+
+TEST(etsi_its_msgs, test_j2735_access) {
+
+  using namespace etsi_its_msgs::J2735_access;
+
+  TestHelper helper;
+
+  IntersectionGeometry intsct;
+
+  unsigned int intersection_id = helper.randomInt(IntersectionID::MIN, IntersectionID::MAX);
+  setIntersectionID(intsct, intersection_id);
+  EXPECT_EQ(intersection_id, getIntersectionID(intsct));
+
+  // Set specific position to test utm projection
+  double latitude = 50.787467;
+  double longitude = 6.046498;
+  double altitude = 209.0;
+  setPosition3D(intsct, latitude, longitude, altitude);
+  int zone;
+  bool northp;
+  gm::PointStamped utm = getUTMPosition(intsct, zone, northp);
+  EXPECT_NEAR(291827.02, utm.point.x, 1e-1);
+  EXPECT_NEAR(5630349.72, utm.point.y, 1e-1);
+  EXPECT_EQ(altitude, utm.point.z);
+  EXPECT_EQ(32, zone);
+  EXPECT_EQ(true, northp);
+  setPosition3DFromUTMPosition(intsct.ref_point, utm, zone, northp);
+  EXPECT_NEAR(latitude, getLatitude(intsct.ref_point), 1e-7);
+  EXPECT_NEAR(longitude, getLongitude(intsct.ref_point), 1e-7);
+  EXPECT_NEAR(altitude, getElevation(intsct.ref_point), 1e-2);
+  
+}
--- etsi_its_msgs_utils/test/impl/test_mapem_access.cpp
+++ etsi_its_msgs_utils/test/impl/test_mapem_access.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cmath>
 #include <limits>
 #include <random>
@@ -6,24 +8,17 @@
 
 #include <gtest/gtest.h>
 
-std::default_random_engine random_engine;
+#include "test_helpers.h"
+#include "test_j2735_access.h"
 
-
-double randomDouble(double min, double max) {
-  std::uniform_real_distribution<double> uniform_distribution_double(min, max);
-  return uniform_distribution_double(random_engine);
-}
-
-int randomInt(int min, int max) {
-  std::uniform_int_distribution<int> uniform_distribution_int(min, max);
-  return uniform_distribution_int(random_engine);
-}
 
 TEST(etsi_its_mapem_msgs, test_set_get_mapem) {
 
+  TestHelper helper;
+
   MAPEM mapem;
 
-  unsigned int moy = randomInt(MinuteOfTheYear::MIN,MinuteOfTheYear::MAX);
+  unsigned int moy = helper.randomInt(MinuteOfTheYear::MIN,MinuteOfTheYear::MAX);
   setMinuteOfTheYear(mapem, moy);
   EXPECT_EQ(moy, getMinuteOfTheYearValue(mapem));
   // Generate dummy time: 04.01.2007 1:15
@@ -42,7 +37,7 @@ TEST(etsi_its_mapem_msgs, test_set_get_mapem) {
   timeinfo.tm_mday = 1;
   timeinfo.tm_mon = 0;
   timeinfo.tm_year = 107; //years since 1900
-  EXPECT_EQ(mktime(&timeinfo)+60*moy, getUnixNanosecondsFromMinuteOfTheYear(mapem, unix_stamp));
+  EXPECT_EQ((mktime(&timeinfo)+60*moy)*1e9, getUnixNanoseconds(mapem, unix_stamp*1e9));
   
 }
 
