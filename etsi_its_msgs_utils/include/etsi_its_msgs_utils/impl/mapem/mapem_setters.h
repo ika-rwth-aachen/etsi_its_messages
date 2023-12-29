d2450799f57f98f6851fd8f07519eec91690b9ef
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2023-12-29 14:23:36 +0100

    move access functions which data-types are not part of spatem to mapem access

--- etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/mapem/mapem_setters.h
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/mapem/mapem_setters.h
@@ -37,13 +37,136 @@ namespace etsi_its_mapem_msgs {
 namespace access {
 
   /**
+   * @brief Set the MinuteOfTheYear for a given MapData object
+   * 
+   * @param map MapData object
+   * @param moy_value value to set
+   */
+  inline void setMinuteOfTheYear(MapData& map, const uint32_t moy_value) {
+    J2735::setMinuteOfTheYear(map.time_stamp, moy_value);
+  }
+
+  /**
    * @brief Set the Minute Of The Year object
    * 
    * @param mapem 
    */
   inline void setMinuteOfTheYear(MAPEM& mapem, const uint32_t moy_value) {
-    J2735::setMinuteOfTheYear(mapem.map, moy_value);
+    setMinuteOfTheYear(mapem.map, moy_value);
     mapem.map.time_stamp_is_present = true;
+  }
+
+  /**
+   * @brief Set the IntersectionID for an IntersectionGeometry object
+   * 
+   * @param intsct IntersectionGeometry object
+   * @param id_value value to set
+   */
+  inline void setIntersectionID(IntersectionGeometry& intsct, const uint16_t id_value) {
+    J2735::setIntersectionID(intsct.id.id, id_value);
+  }
+
+  /**
+   * @brief Set the Latitude object
+   *
+   * @param latitude object to set
+   * @param deg Latitude value in degree as decimal number
+   */
+  inline void setLatitude(Latitude& latitude, const double deg) {
+    int64_t angle_in_10_micro_degree = (int64_t)std::round(deg*1e7);
+    etsi_its_msgs::throwIfOutOfRange(angle_in_10_micro_degree, Latitude::MIN, Latitude::MAX, "Latitude");
+    latitude.value = angle_in_10_micro_degree;
+  }
+
+  /**
+   * @brief Set the Longitude object
+   *
+   * @param longitude object to set
+   * @param deg Longitude value in degree as decimal number
+   */
+  inline void setLongitude(Longitude& longitude, const double deg) {
+    int64_t angle_in_10_micro_degree = (int64_t)std::round(deg*1e7);
+    etsi_its_msgs::throwIfOutOfRange(angle_in_10_micro_degree, Longitude::MIN, Longitude::MAX, "Longitude");
+    longitude.value = angle_in_10_micro_degree;
+  }
+
+  /**
+   * @brief Set the Elevation object
+   *
+   * @param elevation object to set
+   * @param value Elevation value (above the reference ellipsoid surface) in meter as decimal number
+   */
+  inline void setElevation(Elevation& elevation, const double value) {
+    int64_t alt_in_dm = (int64_t)std::round(value*1e1);
+    if(alt_in_dm>=Elevation::MIN && alt_in_dm<=Elevation::MAX) elevation.value = alt_in_dm;
+    else if(alt_in_dm<Elevation::MIN) elevation.value = Elevation::MIN;
+    else if(alt_in_dm>Elevation::MAX) elevation.value = Elevation::MAX;
+  }
+
+  /**
+   * @brief Set the Position3D object
+   *
+   * @param pos object to set
+   * @param latitude Latitude value in degree as decimal number
+   * @param longitude Longitude value in degree as decimal number
+   */
+  inline void setPosition3D(Position3D& pos, const double latitude, const double longitude) {
+    setLatitude(pos.lat, latitude);
+    setLongitude(pos.lon, longitude);
+    pos.elevation_is_present = false;
+  }
+
+  /**
+   * @brief Set the Position3D object
+   *
+   * @param pos object to set
+   * @param latitude Latitude value in degree as decimal number
+   * @param longitude Longitude value in degree as decimal number
+   * @param altitude Altitude value (above the reference ellipsoid surface) in meter as decimal number
+   */
+  inline void setPosition3D(Position3D& pos, const double latitude, const double longitude, const double altitude) {
+    setPosition3D(pos, latitude, longitude);
+    setElevation(pos.elevation, altitude);
+    pos.elevation_is_present = true;
+  }
+
+    /**
+   * @brief Set the Position3D of IntersectionGeometry object
+   *
+   * @param intsct IntersectionGeometry object
+   * @param latitude Latitude value in degree as decimal number
+   * @param longitude Longitude value in degree as decimal number
+   * @param altitude Altitude value (above the reference ellipsoid surface) in meter as decimal number
+   */
+  inline void setPosition3D(IntersectionGeometry& intsct, double latitude, double longitude, double altitude) {
+    setPosition3D(intsct.ref_point, latitude, longitude, altitude);
+  }
+
+  /**
+   * @brief Set the Position3D from a given UTM-Position
+   *
+   * The position is transformed to latitude and longitude by using GeographicLib::UTMUPS
+   * The z-Coordinate is directly used as altitude value
+   * The frame_id of the given utm_position must be set to 'utm_<zone><N/S>'
+   *
+   * @param[out] reference_position Position3D to set
+   * @param[in] utm_position geometry_msgs::PointStamped describing the given utm position
+   * @param[in] zone the UTM zone (zero means UPS) of the given position
+   * @param[in] northp hemisphere (true means north, false means south)
+   */
+  inline void setPosition3DFromUTMPosition(Position3D& reference_position, const gm::PointStamped& utm_position, const int zone, const bool northp) {
+    std::string required_frame_prefix = "utm_";
+    if(utm_position.header.frame_id.rfind(required_frame_prefix, 0) != 0)
+    {
+      throw std::invalid_argument("Frame-ID of UTM Position '"+utm_position.header.frame_id+"' does not start with required prefix '"+required_frame_prefix+"'!");
+    }
+    double latitude, longitude;
+    try {
+      GeographicLib::UTMUPS::Reverse(zone, northp, utm_position.point.x, utm_position.point.y, latitude, longitude);
+    } catch (GeographicLib::GeographicErr& e) {
+      throw std::invalid_argument(e.what());
+    }
+    setPosition3D(reference_position, latitude, longitude, utm_position.point.z);
   }
 
 } // namespace access
--- etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/mapem/mapem_utils.h
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/mapem/mapem_utils.h
@@ -39,15 +39,114 @@ namespace etsi_its_mapem_msgs {
 namespace access {
 
   /**
+   * @brief Get the unix nanoseconds from MapData object
+   * 
+   * @param map given MapData object
+   * @param unix_nanoseconds_estimate unix timestamp to derive the current year from in nanoseconds
+   * @return uint64_t unix timestamp according to the given MinuteOfTheYear in nanoseconds
+   */
+  inline uint64_t getUnixNanosecondsFromMapData(const MapData& map, const uint64_t unix_nanoseconds_estimate) {
+    return J2735::getUnixNanosecondsFromMinuteOfTheYear(getMinuteOfTheYear(map), unix_nanoseconds_estimate);
+  }
+
+  /**
    * @brief Get the unix nanoseconds from MinuteOfTheYear object
    * 
    * @param mapem given MAPEM object
    * @param unix_nanoseconds_estimate unix timestamp to derive the current year from in nanoseconds
    * @return uint64_t unix timestamp according to the stored MinuteOfTheYear in nanoseconds
    */
-  inline uint64_t getUnixNanoseconds(const MAPEM& mapem, const uint64_t unix_timestamp_estimate)
-  {
-    return J2735::getUnixNanosecondsFromMapData(mapem.map, unix_timestamp_estimate);
+  inline uint64_t getUnixNanoseconds(const MAPEM& mapem, const uint64_t unix_timestamp_estimate) {
+    return getUnixNanosecondsFromMapData(mapem.map, unix_timestamp_estimate);
+  }
+
+  /**
+   * @brief Get the UTM Position defined by the given Position3D
+   *
+   * The position is transformed into UTM by using GeographicLib::UTMUPS
+   * The altitude value is directly used as z-Coordinate
+   *
+   * @param[in] reference_position Position3D to get the UTM Position from
+   * @param[out] zone the UTM zone (zero means UPS)
+   * @param[out] northp hemisphere (true means north, false means south)
+   * @return gm::PointStamped geometry_msgs::PointStamped of the given position
+   */
+  inline gm::PointStamped getUTMPosition(const Position3D& reference_position, int& zone, bool& northp) {
+    gm::PointStamped utm_point;
+    double latitude = getLatitude(reference_position.lat);
+    double longitude = getLongitude(reference_position.lon);
+    if(reference_position.elevation_is_present) utm_point.point.z = getElevation(reference_position.elevation);
+    try {
+      GeographicLib::UTMUPS::Forward(latitude, longitude, zone, northp, utm_point.point.x, utm_point.point.y);
+      std::string hemisphere;
+      if(northp) hemisphere="N";
+      else hemisphere="S";
+      utm_point.header.frame_id="utm_"+std::to_string(zone)+hemisphere;
+    } catch (GeographicLib::GeographicErr& e) {
+      throw std::invalid_argument(e.what());
+    }
+    return utm_point;
+  }
+
+    /**
+   * @brief Get the UTM Position defined by the given Position3D along with the grid-convergence angle
+   *
+   * The position is transformed into UTM by using GeographicLib::UTMUPS
+   * The altitude value is directly used as z-Coordinate
+   *
+   * @param[in] reference_position Position3D to get the UTM Position from
+   * @param[out] zone the UTM zone (zero means UPS)
+   * @param[out] northp hemisphere (true means north, false means south)
+   * @param[out] conv_angle grid-convergence angle in degree
+   * @return gm::PointStamped geometry_msgs::PointStamped of the given position
+   */
+  inline gm::PointStamped getUTMPositionWithConvergenceAngle(const Position3D& reference_position, int& zone, bool& northp, double& conv_angle) {
+    gm::PointStamped utm_point;
+    double latitude = getLatitude(reference_position.lat);
+    double longitude = getLongitude(reference_position.lon);
+    if(reference_position.elevation_is_present) utm_point.point.z = getElevation(reference_position.elevation);
+    try {
+      double scale;
+      GeographicLib::UTMUPS::Forward(latitude, longitude, zone, northp, utm_point.point.x, utm_point.point.y, conv_angle, scale);
+      std::string hemisphere;
+      if(northp) hemisphere="N";
+      else hemisphere="S";
+      utm_point.header.frame_id="utm_"+std::to_string(zone)+hemisphere;
+    } catch (GeographicLib::GeographicErr& e) {
+      throw std::invalid_argument(e.what());
+    }
+    return utm_point;
+  }
+
+  /**
+   * @brief Get the UTM Position of ref_point defined by the Position3D along with the grid-convergence angle in an IntersectionGeometry object
+   *
+   * The position is transformed into UTM by using GeographicLib::UTMUPS
+   * The altitude value is directly used as z-Coordinate
+   *
+   * @param[in] intsctn IntersectionGeometry to get the UTM Position from
+   * @param[out] zone the UTM zone (zero means UPS)
+   * @param[out] northp hemisphere (true means north, false means south)
+   * @return gm::PointStamped geometry_msgs::PointStamped of the given position
+   */
+  inline gm::PointStamped getRefPointUTMPosition(const IntersectionGeometry& intsctn, int& zone, bool& northp) {
+    return getUTMPosition(intsctn.ref_point, zone, northp);
+  }
+
+    /**
+   * @brief Get the UTM Position of ref_point defined by the Position3D in an IntersectionGeometry object
+   *
+   * The position is transformed into UTM by using GeographicLib::UTMUPS
+   * The altitude value is directly used as z-Coordinate
+   *
+   * @param[in] intsctn IntersectionGeometry to get the UTM Position from
+   * @param[out] zone the UTM zone (zero means UPS)
+   * @param[out] northp hemisphere (true means north, false means south)
+   * @param[out] conv_angle grid-convergence angle in degree
+   * @return gm::PointStamped geometry_msgs::PointStamped of the given position
+   */
+  inline gm::PointStamped getRefPointUTMPositionWithConvergenceAngle(const IntersectionGeometry& intsctn, int& zone, bool& northp, double& conv_angle) {
+    return getUTMPositionWithConvergenceAngle(intsctn.ref_point, zone, northp, conv_angle);
   }
 
 } // namespace etsi_its_mapem_msgs
