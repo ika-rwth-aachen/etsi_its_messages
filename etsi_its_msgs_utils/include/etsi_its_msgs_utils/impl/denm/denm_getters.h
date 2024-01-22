a2991178cd116ea8cddf059303127656751a8317
Cilli Danker <cilli.danker@ika.rwth-aachen.de>
2024-01-22 18:07:36 +0100

    access map for cause codes

--- etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/denm/denm_getters.h
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/denm/denm_getters.h
@@ -211,32 +211,32 @@ namespace access {
     int cause_code = getCauseCode(denm);
     std::string cause_code_type = "undefined";
 
-    if(cause_code == 1) cause_code_type = "traffic condition";
-    else if(cause_code == 2) cause_code_type = "accident";
-    else if(cause_code == 3) cause_code_type = "roadworks";
-    else if(cause_code == 5) cause_code_type = "impassibility";
-    else if(cause_code == 6) cause_code_type = "adverse weather condition - adhesion";
-    else if(cause_code == 7) cause_code_type = "aquaplanning";
-    else if(cause_code == 9) cause_code_type = "hazardous location - surface condition";
-    else if(cause_code == 10) cause_code_type = "hazardous location - obstacle on the road";
-    else if(cause_code == 11) cause_code_type = "hazardous location - animal on the road";
-    else if(cause_code == 12) cause_code_type = "human presence on the road";
-    else if(cause_code == 14) cause_code_type = "wrong way driving";
-    else if(cause_code == 15) cause_code_type = "rescue and recovery in progress";
-    else if(cause_code == 17) cause_code_type = "adverse weather condition - extreme weather condition";
-    else if(cause_code == 18) cause_code_type = "adverse weather condition - visibility";
-    else if(cause_code == 19) cause_code_type = "adverse weather condition - precipitation";
-    else if(cause_code == 26) cause_code_type = "slow vehicle";
-    else if(cause_code == 27) cause_code_type = "dangerous end of queue";
-    else if(cause_code == 91) cause_code_type = "vehicle breakdown";
-    else if(cause_code == 92) cause_code_type = "post crash";
-    else if(cause_code == 93) cause_code_type = "human problem";
-    else if(cause_code == 94) cause_code_type = "stationary vehicle";
-    else if(cause_code == 95) cause_code_type = "emergency vehicle approaching";
-    else if(cause_code == 96) cause_code_type = "hazardous location - dangerous curve";
-    else if(cause_code == 97) cause_code_type = "collision risk";
-    else if(cause_code == 98) cause_code_type = "signal violation";
-    else if(cause_code == 99) cause_code_type = "dangerous situation";
+    if(cause_code == CauseCodeType().TRAFFIC_CONDITION) cause_code_type = "traffic condition";
+    else if(cause_code == CauseCodeType().ACCIDENT) cause_code_type = "accident";
+    else if(cause_code == CauseCodeType().ROADWORKS) cause_code_type = "roadworks";
+    else if(cause_code == CauseCodeType().IMPASSABILITY) cause_code_type = "impassibility";
+    else if(cause_code == CauseCodeType().ADVERSE_WEATHER_CONDITION_ADHESION) cause_code_type = "adverse weather condition - adhesion";
+    else if(cause_code == CauseCodeType().AQUAPLANNNING) cause_code_type = "aquaplanning";
+    else if(cause_code == CauseCodeType().HAZARDOUS_LOCATION_SURFACE_CONDITION) cause_code_type = "hazardous location - surface condition";
+    else if(cause_code == CauseCodeType().HAZARDOUS_LOCATION_OBSTACLE_ON_THE_ROAD) cause_code_type = "hazardous location - obstacle on the road";
+    else if(cause_code == CauseCodeType().HAZARDOUS_LOCATION_ANIMAL_ON_THE_ROAD) cause_code_type = "hazardous location - animal on the road";
+    else if(cause_code == CauseCodeType().HUMAN_PRESENCE_ON_THE_ROAD) cause_code_type = "human presence on the road";
+    else if(cause_code == CauseCodeType().WRONG_WAY_DRIVING) cause_code_type = "wrong way driving";
+    else if(cause_code == CauseCodeType().RESCUE_AND_RECOVERY_WORK_IN_PROGRESS) cause_code_type = "rescue and recovery in progress";
+    else if(cause_code == CauseCodeType().ADVERSE_WEATHER_CONDITION_EXTREME_WEATHER_CONDITION) cause_code_type = "adverse weather condition - extreme weather condition";
+    else if(cause_code == CauseCodeType().ADVERSE_WEATHER_CONDITION_VISIBILITY) cause_code_type = "adverse weather condition - visibility";
+    else if(cause_code == CauseCodeType().ADVERSE_WEATHER_CONDITION_PRECIPITATION) cause_code_type = "adverse weather condition - precipitation";
+    else if(cause_code == CauseCodeType().SLOW_VEHICLE) cause_code_type = "slow vehicle";
+    else if(cause_code == CauseCodeType().DANGEROUS_END_OF_QUEUE) cause_code_type = "dangerous end of queue";
+    else if(cause_code == CauseCodeType().VEHICLE_BREAKDOWN) cause_code_type = "vehicle breakdown";
+    else if(cause_code == CauseCodeType().POST_CRASH) cause_code_type = "post crash";
+    else if(cause_code == CauseCodeType().HUMAN_PROBLEM) cause_code_type = "human problem";
+    else if(cause_code == CauseCodeType().STATIONARY_VEHICLE) cause_code_type = "stationary vehicle";
+    else if(cause_code == CauseCodeType().EMERGENCY_VEHICLE_APPROACHING) cause_code_type = "emergency vehicle approaching";
+    else if(cause_code == CauseCodeType().HAZARDOUS_LOCATION_DANGEROUS_CURVE) cause_code_type = "hazardous location - dangerous curve";
+    else if(cause_code == CauseCodeType().COLLISION_RISK) cause_code_type = "collision risk";
+    else if(cause_code == CauseCodeType().SIGNAL_VIOLATION) cause_code_type = "signal violation";
+    else if(cause_code == CauseCodeType().DANGEROUS_SITUATION) cause_code_type = "dangerous situation";
 
     return cause_code_type;
   }
@@ -253,7 +253,7 @@ namespace access {
     int cause_code = getCauseCode(denm);
     int sub_cause_code = getSubCauseCode(denm);
     std::string sub_cause_code_type = "undefined";
-    if(cause_code == 1) {
+    if(cause_code == CauseCodeType().TRAFFIC_CONDITION) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "not defined";
       else if(sub_cause_code == 2) sub_cause_code_type = "traffic jam slowly increasing";
@@ -264,70 +264,70 @@ namespace access {
       else if(sub_cause_code == 7) sub_cause_code_type = "traffic jam decreasing";
       else if(sub_cause_code == 8) sub_cause_code_type = "traffic jam strongly decreasing";
       }
-    else if(cause_code == 2) {
+    else if(cause_code == CauseCodeType().ACCIDENT) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 7) sub_cause_code_type = "not defined";
       else if(sub_cause_code == 8) sub_cause_code_type = "assistance requested (e-Call)";
       }
-    else if(cause_code == 3) {
+    else if(cause_code == CauseCodeType().ROADWORKS) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 3) sub_cause_code_type = "not defined";
       else if(sub_cause_code == 4) sub_cause_code_type = "short-term stationary roadworks";
       else if(sub_cause_code == 5) sub_cause_code_type = "street cleaning";
       else if(sub_cause_code == 6) sub_cause_code_type = "winter service";
     }
-    else if(cause_code == 5) sub_cause_code_type = "not defined";
-    else if(cause_code == 6) {
+    else if(cause_code == CauseCodeType().IMPASSABILITY) sub_cause_code_type = "not defined";
+    else if(cause_code == CauseCodeType().ADVERSE_WEATHER_CONDITION_ADHESION) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 10) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 7) sub_cause_code_type = "not defined";
-    else if(cause_code == 9) {
+    else if(cause_code == CauseCodeType().AQUAPLANNNING) sub_cause_code_type = "not defined";
+    else if(cause_code == CauseCodeType().HAZARDOUS_LOCATION_SURFACE_CONDITION) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 9) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 10) {
+    else if(cause_code == CauseCodeType().HAZARDOUS_LOCATION_OBSTACLE_ON_THE_ROAD) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 7) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 11) {
+    else if(cause_code == CauseCodeType().HAZARDOUS_LOCATION_ANIMAL_ON_THE_ROAD) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 4) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 12) {
+    else if(cause_code == CauseCodeType().HUMAN_PRESENCE_ON_THE_ROAD) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 3) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 14) {
+    else if(cause_code == CauseCodeType().WRONG_WAY_DRIVING) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "vehicle driving in wrong lane";
       else if(sub_cause_code == 2) sub_cause_code_type = "vehicle driving in wrong driving direction";
       }
-    else if(cause_code == 15) {
+    else if(cause_code == CauseCodeType().RESCUE_AND_RECOVERY_WORK_IN_PROGRESS) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 5) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 17) {
+    else if(cause_code == CauseCodeType().ADVERSE_WEATHER_CONDITION_EXTREME_WEATHER_CONDITION) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 6) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 18) {
+    else if(cause_code == CauseCodeType().ADVERSE_WEATHER_CONDITION_VISIBILITY) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 8) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 19) {
+    else if(cause_code == CauseCodeType().ADVERSE_WEATHER_CONDITION_PRECIPITATION) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 3) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 26) {
+    else if(cause_code == CauseCodeType().SLOW_VEHICLE) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 8) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 27) {
+    else if(cause_code == CauseCodeType().DANGEROUS_END_OF_QUEUE) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code >= 1 && sub_cause_code <= 8) sub_cause_code_type = "not defined";
       }
-    else if(cause_code == 91) {
+    else if(cause_code == CauseCodeType().VEHICLE_BREAKDOWN) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "lack of fuel";
       else if(sub_cause_code == 2) sub_cause_code_type = "lack of battery";
@@ -338,19 +338,19 @@ namespace access {
       else if(sub_cause_code == 7) sub_cause_code_type = "steering problem";
       else if(sub_cause_code == 8) sub_cause_code_type = "tyre puncture";
       }
-    else if(cause_code == 92) {
+    else if(cause_code == CauseCodeType().POST_CRASH) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "accident without e-Call triggered";
       else if(sub_cause_code == 2) sub_cause_code_type = "accident with e-Call manually triggered";
       else if(sub_cause_code == 3) sub_cause_code_type = "accident with e-Call automatical triggered";
       else if(sub_cause_code == 4) sub_cause_code_type = "accident with e-Call triggered without a possible access to a cell network";
       }
-    else if(cause_code == 93) {
+    else if(cause_code == CauseCodeType().HUMAN_PROBLEM) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "glycaemia problem";
       else if(sub_cause_code == 2) sub_cause_code_type = "heart problem";
       }
-    else if(cause_code == 94) {
+    else if(cause_code == CauseCodeType().STATIONARY_VEHICLE) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "human problem";
       else if(sub_cause_code == 2) sub_cause_code_type = "vehicle breakdown";
@@ -358,12 +358,12 @@ namespace access {
       else if(sub_cause_code == 4) sub_cause_code_type = "public transport stop";
       else if(sub_cause_code == 5) sub_cause_code_type = "carrying dangerous goods";
       }
-    else if(cause_code == 95) {
+    else if(cause_code == CauseCodeType().EMERGENCY_VEHICLE_APPROACHING) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "emergency vehicle approaching";
       else if(sub_cause_code == 2) sub_cause_code_type = "prioritized vehicle approaching";
       }
-    else if(cause_code == 96) {
+    else if(cause_code == CauseCodeType().HAZARDOUS_LOCATION_DANGEROUS_CURVE) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "dangerous left turn curve";
       else if(sub_cause_code == 2) sub_cause_code_type = "dangerous right turn curve";
@@ -371,20 +371,20 @@ namespace access {
       else if(sub_cause_code == 4) sub_cause_code_type = "multiple curves starting with left turn";
       else if(sub_cause_code == 5) sub_cause_code_type = "multiple curves starting with right turn";
       }
-    else if(cause_code == 97) {
+    else if(cause_code == CauseCodeType().COLLISION_RISK) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "longitudinal collision risk";
       else if(sub_cause_code == 2) sub_cause_code_type = "crossing collision risk";
       else if(sub_cause_code == 3) sub_cause_code_type = "lateral collision risk";
       else if(sub_cause_code == 4) sub_cause_code_type = "collision risk involving vulnerable road user";
       }
-    else if(cause_code == 98) {
+    else if(cause_code == CauseCodeType().SIGNAL_VIOLATION) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "stop sign violation";
       else if(sub_cause_code == 2) sub_cause_code_type = "traffic light violation";
       else if(sub_cause_code == 3) sub_cause_code_type = "turning regulation violation";
       }
-    else if(cause_code == 99) {
+    else if(cause_code == CauseCodeType().DANGEROUS_SITUATION) {
       if(sub_cause_code == 0) sub_cause_code_type = "unavailable";
       else if(sub_cause_code == 1) sub_cause_code_type = "emergency electronic break lights";
       else if(sub_cause_code == 2) sub_cause_code_type = "pre-crash system activated";
