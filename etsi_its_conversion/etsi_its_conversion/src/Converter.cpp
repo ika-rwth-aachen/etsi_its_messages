74d8c2f9a39921efc168cf998286449f692a5b47
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-07-10 23:17:30 +0200

    remove duplicate code in converter.cpp

--- etsi_its_conversion/etsi_its_conversion/src/Converter.cpp
+++ etsi_its_conversion/etsi_its_conversion/src/Converter.cpp
@@ -44,54 +44,6 @@ PLUGINLIB_EXPORT_CLASS(etsi_its_conversion::Converter, nodelet::Nodelet)
 RCLCPP_COMPONENTS_REGISTER_NODE(etsi_its_conversion::Converter)
 #endif
 
-
-/** ============================================================================
-MIT License
-
-Copyright (c) 2023 Institute for Automotive Engineering (ika), RWTH Aachen University
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-============================================================================= */
-
-#include <algorithm>
-#include <arpa/inet.h>
-#include <sstream>
-
-#ifdef ROS1
-#include <ros/console.h>
-#else
-#include <rcutils/logging.h>
-#endif
-
-#include "etsi_its_conversion/Converter.hpp"
-
-#ifdef ROS1
-#define ROS12_LOG(level, ...) NODELET_##level(__VA_ARGS__)
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(etsi_its_conversion::Converter, nodelet::Nodelet)
-#else
-#define ROS12_LOG(level, ...) RCLCPP_##level(this->get_logger(), __VA_ARGS__)
-#include <rclcpp_components/register_node_macro.hpp>
-RCLCPP_COMPONENTS_REGISTER_NODE(etsi_its_conversion::Converter)
-#endif
-
-
 namespace etsi_its_conversion {
 
 const int kBtpHeaderDestinationPortCam{2001};
