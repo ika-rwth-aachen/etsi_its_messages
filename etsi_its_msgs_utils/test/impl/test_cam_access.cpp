9a7ddfd2a68fa5ae0c5ebe1f85e5d70383b0fd4d
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-07-10 18:26:35 +0200

    refactor etsi_its_msgs_utils

--- etsi_its_msgs_utils/test/impl/test_cam_access.cpp
+++ etsi_its_msgs_utils/test/impl/test_cam_access.cpp
@@ -38,13 +38,13 @@ TEST(etsi_its_cam_msgs, test_set_get_cam) {
   uint64_t t_2007 = ((uint64_t)1167609600)*1e9;
   TimestampIts t_its;
   EXPECT_EQ(1, etsi_its_msgs::getLeapSecondInsertionsSince2004(t_2007*1e-9));
-  etsi_its_msgs::cdd_access::setTimestampITS(t_its, t_2007, etsi_its_msgs::getLeapSecondInsertionsSince2004(t_2007*1e-9));
+  etsi_its_cam_msgs::access::setTimestampITS(t_its, t_2007, etsi_its_msgs::getLeapSecondInsertionsSince2004(t_2007*1e-9));
   EXPECT_EQ(94694401000, t_its.value);
   setGenerationDeltaTime(cam, t_2007, etsi_its_msgs::getLeapSecondInsertionsSince2004(t_2007*1e-9));
   EXPECT_EQ(94694401000%65536, getGenerationDeltaTimeValue(cam));
   TimestampIts t_its2;
   uint64_t t_2007_off = t_2007 + 5*1e9;
-  etsi_its_msgs::cdd_access::setTimestampITS(t_its2, t_2007_off, etsi_its_msgs::getLeapSecondInsertionsSince2004(t_2007*1e-9));
+  etsi_its_cam_msgs::access::setTimestampITS(t_its2, t_2007_off, etsi_its_msgs::getLeapSecondInsertionsSince2004(t_2007*1e-9));
   EXPECT_EQ(94694401000, getTimestampITSFromGenerationDeltaTime(getGenerationDeltaTime(cam), t_its2).value);
   EXPECT_EQ(t_2007, getUnixNanosecondsFromGenerationDeltaTime(getGenerationDeltaTime(cam), t_its2, etsi_its_msgs::getLeapSecondInsertionsSince2004(t_2007*1e-9)));
   EXPECT_EQ(t_2007, getUnixNanosecondsFromGenerationDeltaTime(getGenerationDeltaTime(cam), t_2007_off, etsi_its_msgs::getLeapSecondInsertionsSince2004(t_2007*1e-9)));
