db595027b26892166fe686b56d9fa5b97f3eb5e5
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-07-26 15:17:13 +0000

    add demo cam_ts publisher script

--- utils/scripts/publish_cam_ts.py
+++ utils/scripts/publish_cam_ts.py
@@ -26,15 +26,15 @@
 
 import rclpy
 from rclpy.node import Node
-from etsi_its_cam_msgs.msg import *
+from etsi_its_cam_ts_msgs.msg import *
 
 
 class Publisher(Node):
 
     def __init__(self):
 
-        super().__init__("cam_publisher")
-        topic = "/etsi_its_conversion/cam/in"
+        super().__init__("cam_ts_publisher")
+        topic = "/etsi_its_conversion/cam_ts/in"
         self.publisher = self.create_publisher(CAM, topic, 1)
         self.timer = self.create_timer(1.0, self.publish)
 
@@ -42,26 +42,26 @@ class Publisher(Node):
 
         msg = CAM()
 
-        msg.header.protocol_version = 2
-        msg.header.message_id = msg.header.MESSAGE_ID_CAM
+        msg.header.protocol_version.value = 2
+        msg.header.message_id.value = msg.header.message_id.CAM
         msg.header.station_id.value = 32
 
         msg.cam.generation_delta_time.value = msg.cam.generation_delta_time.ONE_MILLI_SEC
 
         msg.cam.cam_parameters.basic_container.station_type.value = msg.cam.cam_parameters.basic_container.station_type.PASSENGER_CAR
-        msg.cam.cam_parameters.basic_container.reference_position.latitude.value = int(msg.cam.cam_parameters.basic_container.reference_position.latitude.ONE_MICRODEGREE_NORTH * 1e6 * 51.215169611787054)
+        msg.cam.cam_parameters.basic_container.reference_position.latitude.value = int(1e7 * 51.215169611787054)
 
         basic_vehicle_container_high_frequency = BasicVehicleContainerHighFrequency()
         basic_vehicle_container_high_frequency.heading.heading_value.value = basic_vehicle_container_high_frequency.heading.heading_value.WGS84_NORTH
-        basic_vehicle_container_high_frequency.heading.heading_confidence.value = basic_vehicle_container_high_frequency.heading.heading_confidence.EQUAL_OR_WITHIN_ONE_DEGREE
-        basic_vehicle_container_high_frequency.speed.speed_value.value = basic_vehicle_container_high_frequency.speed.speed_value.ONE_CENTIMETER_PER_SEC
-        basic_vehicle_container_high_frequency.speed.speed_confidence.value = basic_vehicle_container_high_frequency.speed.speed_confidence.EQUAL_OR_WITHIN_ONE_CENTIMETER_PER_SEC
-        basic_vehicle_container_high_frequency.vehicle_length.vehicle_length_value.value = basic_vehicle_container_high_frequency.vehicle_length.vehicle_length_value.TEN_CENTIMETERS
-        basic_vehicle_container_high_frequency.vehicle_width.value = basic_vehicle_container_high_frequency.vehicle_width.TEN_CENTIMETERS
+        basic_vehicle_container_high_frequency.heading.heading_confidence.value = basic_vehicle_container_high_frequency.heading.heading_confidence.MIN
+        basic_vehicle_container_high_frequency.speed.speed_value.value = 1
+        basic_vehicle_container_high_frequency.speed.speed_confidence.value = basic_vehicle_container_high_frequency.speed.speed_confidence.MIN
+        basic_vehicle_container_high_frequency.vehicle_length.vehicle_length_value.value = basic_vehicle_container_high_frequency.vehicle_length.vehicle_length_value.MIN
+        basic_vehicle_container_high_frequency.vehicle_width.value = basic_vehicle_container_high_frequency.vehicle_width.MIN
         msg.cam.cam_parameters.high_frequency_container.choice = msg.cam.cam_parameters.high_frequency_container.CHOICE_BASIC_VEHICLE_CONTAINER_HIGH_FREQUENCY
         msg.cam.cam_parameters.high_frequency_container.basic_vehicle_container_high_frequency = basic_vehicle_container_high_frequency
 
-        self.get_logger().info(f"Publishing CAM")
+        self.get_logger().info(f"Publishing CAM (TS)")
         self.publisher.publish(msg)
 
 
