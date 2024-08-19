5c4aa17e337f7a58cc1c30eb15b26dec8c85fd27
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2024-08-19 12:20:20 +0200

    add vam-ts coding

---
+++ etsi_its_coding/etsi_its_vam_ts_coding/src/per_support.c
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2005-2017 Lev Walkin <vlm@lionet.info>. All rights reserved.
+ * Redistribution and modifications are permitted subject to BSD license.
+ */
+#include <etsi_its_vam_ts_coding/asn_system.h>
+#include <etsi_its_vam_ts_coding/asn_internal.h>
+#include <etsi_its_vam_ts_coding/per_support.h>
+
+// Absolutely nothing
