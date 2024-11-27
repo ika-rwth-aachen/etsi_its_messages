253ceae010cdaf3e3617eb53bd7ba40764129271
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2024-11-27 15:41:02 +0000

    include mapem tests into new test structure

--- etsi_its_msgs_utils/test/test_access.ros2.cpp
+++ etsi_its_msgs_utils/test/test_access.ros2.cpp
@@ -14,6 +14,9 @@
 #include <etsi_its_denm_msgs/msg/denm.hpp>
 #include <etsi_its_msgs_utils/denm_access.hpp>
 
+#include <etsi_its_mapem_ts_msgs/msg/mapem.hpp>
+#include <etsi_its_msgs_utils/mapem_ts_access.hpp>
+
 std::default_random_engine random_engine;
 double randomDouble(double min, double max) {
   std::uniform_real_distribution<double> uniform_distribution_double(min, max);
@@ -37,6 +40,9 @@ namespace cpm_ts_msgs = etsi_its_cpm_ts_msgs::msg;
 
 namespace denm_msgs = etsi_its_denm_msgs::msg;
 #include <impl/test_denm_access.cpp>
+
+namespace mapem_ts_msgs = etsi_its_mapem_ts_msgs::msg;
+#include <impl/test_mapem_ts_access.cpp>
 
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
--- etsi_its_msgs_utils/test/test_mapem_access.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <etsi_its_mapem_msgs/MAPEM.h>
-
-#include <etsi_its_msgs_utils/mapem_access.h>
-
-using namespace etsi_its_mapem_msgs;
-using namespace etsi_its_mapem_msgs::access;
-
-#include <impl/test_mapem_access.cpp>
