76fd2eeb2607270b279383472393ee8f5beb5505
Raphael Riebl <raphael.riebl@nfiniity.com>
2025-05-08 23:44:50 +0200

    update etsi_its_coding

--- etsi_its_coding/etsi_its_mcm_uulm_coding/src/INTEGER.c
+++ etsi_its_coding/etsi_its_mcm_uulm_coding/src/INTEGER.c
@@ -100,7 +100,7 @@ asn_TYPE_descriptor_t asn_DEF_INTEGER = {
  * INTEGER specific human-readable output.
  */
 ssize_t
-INTEGER__dump(const asn_TYPE_descriptor_t *td, const INTEGER_t *st, asn_app_consume_bytes_f *cb, void *app_key, int plainOrXER) {
+INTEGER__dump(const asn_TYPE_descriptor_t *td, const INTEGER_t *st, asn_app_consume_bytes_f *cb, void *app_key, int plainOrXEROrJER) {
     const asn_INTEGER_specifics_t *specs =
         (const asn_INTEGER_specifics_t *)td->specifics;
 	char scratch[32];
@@ -122,13 +122,16 @@ INTEGER__dump(const asn_TYPE_descriptor_t *td, const INTEGER_t *st, asn_app_cons
 		el = (value >= 0 || !specs || !specs->field_unsigned)
 			? INTEGER_map_value2enum(specs, value) : 0;
 		if(el) {
-			if(plainOrXER == 0)
+			if(plainOrXEROrJER == 0)
 				return asn__format_to_callback(cb, app_key,
 					"%" ASN_PRIdMAX " (%s)", value, el->enum_name);
-			else
+			else if (plainOrXEROrJER == 1)
 				return asn__format_to_callback(cb, app_key,
 					"<%s/>", el->enum_name);
-		} else if(plainOrXER && specs && specs->strict_enumeration) {
+			else if (plainOrXEROrJER == 2)
+				return asn__format_to_callback(cb, app_key,
+					"\"%s\"", el->enum_name);
+		} else if(plainOrXEROrJER && specs && specs->strict_enumeration) {
 			ASN_DEBUG("ASN.1 forbids dealing with "
 				"unknown value of ENUMERATED type");
 			errno = EPERM;
@@ -140,7 +143,7 @@ INTEGER__dump(const asn_TYPE_descriptor_t *td, const INTEGER_t *st, asn_app_cons
                                                : "%" ASN_PRIdMAX,
                                            value);
         }
-	} else if(plainOrXER && specs && specs->strict_enumeration) {
+	} else if(plainOrXEROrJER && specs && specs->strict_enumeration) {
 		/*
 		 * Here and earlier, we cannot encode the ENUMERATED values
 		 * if there is no corresponding identifier.
