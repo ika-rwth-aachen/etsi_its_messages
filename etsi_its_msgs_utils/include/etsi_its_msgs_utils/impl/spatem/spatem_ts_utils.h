e42d01d98c7ddc36b21ab3d69486c41912b93030
Alexander Wilczynski <alexander.wilczynski@ika.rwth-aachen.de>
2025-07-17 18:49:08 +0200

    Fix time conversion from seconds to nanoseconds

--- etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/spatem/spatem_ts_utils.h
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/spatem/spatem_ts_utils.h
@@ -237,7 +237,7 @@ inline time_mark_value_interpretation interpretTimeMarkValueType(const uint16_t 
  inline int64_t interpretTimeMarkValueAsNanoSeconds(const uint16_t time, const uint64_t nanosec) {
   // calculate elapsed nanoseconds since the start of the last full hour in nanoseconds 
   int64_t abs_time_hour_nanosec = ((nanosec) % (HOUR_IN_SECONDS * FACTOR_SECONDS_TO_NANOSECONDS));
-  int64_t abs_time_decoded_nanosec = static_cast<int64_t>(time) * FACTOR_ETSI_TIMEMARK_TO_SECONDS;
+  int64_t abs_time_decoded_nanosec = static_cast<int64_t>(time) * FACTOR_ETSI_TIMEMARK_TO_SECONDS * FACTOR_SECONDS_TO_NANOSECONDS;
   int64_t rel_time_until_change =  abs_time_decoded_nanosec - abs_time_hour_nanosec;
 
   // adjust relative time if a jump to the next hour occurs (relative time inside the interval [-30:00, 30:00] )
