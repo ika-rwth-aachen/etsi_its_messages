ca741fe9386097bd9c48c0737a856ea49fe9c3d2
jpbusch <jpbusch@users.noreply.github.com>
2025-07-18 12:30:20 +0000

    deploy: ad939728f229e3d94d186943166bbcd589c5f706

--- spatem__ts__utils_8h.js
+++ spatem__ts__utils_8h.js
@@ -4,7 +4,7 @@ var spatem__ts__utils_8h =
     [ "etsi_its_spatem_ts_msgs::access::getUnixSecondsOfYear", "spatem__ts__utils_8h.html#a05620c9a8ad99267d3dba2a29de26908", null ],
     [ "etsi_its_spatem_ts_msgs::access::interpretMovementPhaseStateAsColor", "spatem__ts__utils_8h.html#a265c9b3e76c31299b5b4d32c6f0d8c08", null ],
     [ "etsi_its_spatem_ts_msgs::access::interpretTimeIntervalConfidenceAsFloat", "spatem__ts__utils_8h.html#aacb7c330be55e7ca055d3d47dbcafd01", null ],
-    [ "etsi_its_spatem_ts_msgs::access::interpretTimeMarkValueAsNanoSeconds", "spatem__ts__utils_8h.html#a43541f4a6593cbfa3a435721c00f0d46", null ],
+    [ "etsi_its_spatem_ts_msgs::access::interpretTimeMarkDeltaTimeAsNanoSeconds", "spatem__ts__utils_8h.html#ad5891cbae0c4c7de6eb051cafb372f01", null ],
     [ "etsi_its_spatem_ts_msgs::access::interpretTimeMarkValueAsSeconds", "spatem__ts__utils_8h.html#a84610814e3088a91f4c6a6e5c076b19b", null ],
     [ "etsi_its_spatem_ts_msgs::access::interpretTimeMarkValueType", "spatem__ts__utils_8h.html#a85b380388e8aa682e7b4bb703061d9e4", null ],
     [ "etsi_its_spatem_ts_msgs::access::parseTimeMarkValueToString", "spatem__ts__utils_8h.html#a9dac29f2eaa5a85ad4fbdf03155755c0", null ]
