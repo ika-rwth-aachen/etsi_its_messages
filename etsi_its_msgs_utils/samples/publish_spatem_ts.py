32c4700b0e944b560de81f5b7b9039e6713e9c44
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2026-04-20 10:43:09 -0300

    Updating modification to latest main version

--- etsi_its_msgs_utils/samples/publish_spatem_ts.py
+++ etsi_its_msgs_utils/samples/publish_spatem_ts.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# Based in https://github.com/ika-rwth-aachen/etsi_its_messages/blob/main/etsi_its_msgs_utils/samples/publish_spatem_ts.py
-
 # ==============================================================================
 # MIT License
 #
@@ -28,8 +26,10 @@
 
 import rclpy
 from rclpy.node import Node
-from etsi_its_spatem_ts_msgs.msg import *
+
 import utils
+from etsi_its_spatem_ts_msgs.msg import *
+from etsi_its_conversion_srvs.srv import ConvertSpatemTsToUdp, ConvertUdpToSpatemTs
 
 # Phase time configuration
 RED_LIGHT_TIME = 15
@@ -37,28 +37,25 @@ YELLOW_LIGHT_TIME = 3
 GREEN_LIGHT_TIME = 10
 #
 
-HOUR_IN_SECONDS = 3600
-FACTOR_SECONDS_TO_NANOSECONDS = 1000000000
-
-FACTOR_ETSI_TIMEMARK_TO_NANOSECONDS = 100000000
-FACTOR_ETSI_TIMEMARK_TO_SECONDS = 0.1
-
 
 class Publisher(Node):
 
     def __init__(self):
 
-        super().__init__("mapem_ts_publisher")
+        super().__init__("spatem_ts_publisher")
+        self.type = "SPATEM_TS"
         topic = "/etsi_its_conversion/spatem_ts/in"
         self.publisher = self.create_publisher(SPATEM, topic, 1)
+        self.srv_to_udp_client = self.create_client(ConvertSpatemTsToUdp, "/etsi_its_conversion/spatem_ts/udp")
+        self.srv_to_ros_client = self.create_client(ConvertUdpToSpatemTs, "/etsi_its_conversion/udp/spatem_ts")
         self.timer = self.create_timer(1.0, self.publish)
+        self.timer_controller = self.create_timer(1.0, self.controller)
         
+        # Initializing controller
         self.state = MovementEvent().event_state.DARK
         self.phasing = 0
-
-    def publish(self):
         
-        self.controller()
+    def buildMessage(self):
 
         msg = SPATEM()
 
@@ -101,9 +98,39 @@ class Publisher(Node):
         intersection_state.states.array.append(movement_state)
         
         msg.spat.intersections.array.append(intersection_state)
+        
+        return msg
 
+    def publish(self):
+        
+        msg = self.buildMessage()
         self.get_logger().info(f"Publishing SPATEM (TS)")
         self.publisher.publish(msg)
+        
+    def callService(self):
+
+        msg = self.buildMessage()
+        srv_request = ConvertSpatemTsToUdp.Request(ros_msg=msg)
+        self.get_logger().info(f"Calling service to convert {self.type} from ROS to UDP")
+        srv_future = self.srv_to_udp_client.call_async(srv_request)
+        while not srv_future.done():
+            rclpy.spin_once(self)
+        result = srv_future.result()
+        if result is None:
+            self.get_logger().error("Service call failed")
+            return
+        self.get_logger().info("Service call succeeded")
+
+        udp_msg = result.udp_packet
+        srv_request = ConvertUdpToSpatemTs.Request(udp_packet=udp_msg)
+        self.get_logger().info(f"Calling service to convert {self.type} from UDP to ROS")
+        srv_future = self.srv_to_ros_client.call_async(srv_request)
+        while not srv_future.done():
+            rclpy.spin_once(self)
+        if srv_future.result() is not None:
+            self.get_logger().info("Service call succeeded")
+        else:
+            self.get_logger().error("Service call failed")
         
     def controller(self):
         self.phasing = self.phasing + 1
@@ -128,5 +155,9 @@ if __name__ == "__main__":
 
     rclpy.init()
     publisher = Publisher()
+    if publisher.srv_to_udp_client.wait_for_service(timeout_sec=1.0) and publisher.srv_to_ros_client.wait_for_service(timeout_sec=1.0):
+        publisher.callService()
+    else:
+        publisher.get_logger().warning("Conversion services not available, skipping ...")
     rclpy.spin(publisher)
     rclpy.shutdown()
