46e0856b82601759b6cc7b2ded7e2c13a940ba75
jpbusch <jpbusch@users.noreply.github.com>
2026-06-06 12:15:40 +0000

    deploy: 6e9bd05aa374ba80b7ce66b0251abca66a228e70

--- navtreedata.js
+++ navtreedata.js
@@ -45,6 +45,6 @@ var NAVTREEINDEX =
 "mapem__ts__utils_8h_source.html"
 ];
 
-var SYNCONMSG = 'click to disable panel synchronization';
-var SYNCOFFMSG = 'click to enable panel synchronization';
-var LISTOFALLMEMBERS = 'List of all members';
+const SYNCONMSG = 'click to disable panel synchronization';
+const SYNCOFFMSG = 'click to enable panel synchronization';
+const LISTOFALLMEMBERS = 'List of all members';
